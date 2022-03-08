cc3f90c907cd9925da4a3eac56063a9c589498a9
zeljkosamed <smdkthyli@gmail.com>
2022-03-09 00:28:36 +0300

    Context oluşturuldu ve ConnectionString appsettings'e eklendi.

---
+++ DataAccess/Concrete/EntityFramework/Contexts/TraversalContext.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class TraversalContext : DbContext
+    {
+        public TraversalContext(DbContextOptions<TraversalContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<AboutPost> AboutPosts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+        public DbSet<Newsletter> Newsletters { get; set; }
+        public DbSet<OtherFeature> OtherFeatures { get; set; }
+        public DbSet<SubAbout> SubAbouts { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -1,6 +1,8 @@
+using DataAccess.Concrete.EntityFramework.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,9 @@ namespace WebUI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<TraversalContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("DataAccess")));
+            services.AddScoped<DbContext>(provider => provider.GetService<TraversalContext>());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
