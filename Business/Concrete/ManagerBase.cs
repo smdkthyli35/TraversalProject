0bc6c5f0d7bc5ad42c4519e10ba4e93596690c44
zeljkosamed <smdkthyli@gmail.com>
2022-03-10 18:02:45 +0300

    ManagerBase ve Messages sınıfı oluşturuldu.

---
+++ Business/Concrete/ManagerBase.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ManagerBase
+    {
+        protected IUnitOfWork UnitOfWork;
+        protected IMapper Mapper;
+
+        public ManagerBase(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            UnitOfWork = unitOfWork;
+            Mapper = mapper;
+        }
+
+    }
+}
---
+++ Business/Utilities/Messages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Utilities
+{
+    public static class Messages
+    {
+        public static class AboutPost
+        {
+            public static string NotFound(bool isPlural)
+            {
+                if (isPlural) return "Hiçbir hakkında içeriği bulunamadı.";
+                return "Böyle bir hakkında içeriği bulunamadı.";
+            }
+
+            public static string Add = "Eklendi";
+            public static string Delete = "Eklendi";
+            public static string Update = "Eklendi";
+        }
+    }
+}
