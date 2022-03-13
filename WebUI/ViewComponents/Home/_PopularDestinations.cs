100cf75395fa728b67766f24bc8c5e6456c12d24
zeljkosamed <smdkthyli@gmail.com>
2022-03-13 23:12:13 +0300

    Tur rotaları listelendi.

---
+++ WebUI/ViewComponents/Home/_PopularDestinations.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebUI.Models;
+
+namespace WebUI.ViewComponents.Home
+{
+    public class _PopularDestinations : ViewComponent
+    {
+        private readonly IDestinationService _destinationService;
+
+        public _PopularDestinations(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var destinations = await _destinationService.GetAllAsync();
+            return View(new PopularDestinationsViewModel
+            {
+                Destinations = destinations.Data.Destinations
+            });
+        }
+    }
+}
