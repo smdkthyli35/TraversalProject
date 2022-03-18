dcedc4f60b44f32d7456bbc94761d437ae1e4d6e
zeljkosamed <smdkthyli@gmail.com>
2022-03-18 22:07:27 +0300

    Anasayfa referanslar bölümü listelendi.

---
+++ WebUI/Models/TestimonialViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Models
+{
+    public class TestimonialViewModel
+    {
+        public IList<Testimonial> Testimonials { get; set; }
+    }
+}
---
+++ WebUI/ViewComponents/Home/_Testimonial.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebUI.Models;
+
+namespace WebUI.ViewComponents.Home
+{
+    public class _Testimonial : ViewComponent
+    {
+        private readonly ITestimonialService _testimonialService;
+
+        public _Testimonial(ITestimonialService testimonialService)
+        {
+            _testimonialService = testimonialService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var testimonials = await _testimonialService.GetAllAsync();
+            return View(new TestimonialViewModel
+            {
+                Testimonials = testimonials.Data.Testimonials
+            });
+        }
+    }
+}
