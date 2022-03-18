bd8ca6ab361cd35dacee1f47696369574e2c343c
zeljkosamed <smdkthyli@gmail.com>
2022-03-18 21:51:45 +0300

    Alt hakkında alanı ui'de listelendi.

---
+++ WebUI/ViewComponents/Home/_SubAbout.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebUI.Models;
+
+namespace WebUI.ViewComponents.Home
+{
+    public class _SubAbout : ViewComponent
+    {
+        private readonly ISubAboutService _subAboutService;
+
+        public _SubAbout(ISubAboutService subAboutService)
+        {
+            _subAboutService = subAboutService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var subAbout = await _subAboutService.GetAllAsync();
+            return View(new SubAboutViewModel
+            {
+                SubAbouts = subAbout.Data.SubAbouts
+            });
+        }
+    }
+}
