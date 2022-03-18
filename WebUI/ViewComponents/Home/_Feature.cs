21152920683923c5f2725c519699e4f4c72dd32f
zeljkosamed <smdkthyli@gmail.com>
2022-03-18 21:18:12 +0300

    İstatistikler getirildi. Özellikler sayfası backendi yazıldı.

---
+++ WebUI/ViewComponents/Home/_Feature.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebUI.Models;
+
+namespace WebUI.ViewComponents.Home
+{
+    public class _Feature : ViewComponent
+    {
+        private readonly IFeatureService _featureService;
+
+        public _Feature(IFeatureService featureService)
+        {
+            _featureService = featureService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var features = await _featureService.GetAllAsync();
+            return View(new FeatureViewModel
+            {
+                Features = features.Data.Features
+            });
+        }
+    }
+}
---
+++ WebUI/ViewComponents/Home/_Statistics.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebUI.Models;
+
+namespace WebUI.ViewComponents.Home
+{
+    public class _Statistics : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public _Statistics(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewBag.destination = await _unitOfWork.Destinations.CountAsync();
+            ViewBag.guide = await _unitOfWork.Guides.CountAsync();
+            ViewBag.customer = "285";
+            return View();
+        }
+    }
+}
