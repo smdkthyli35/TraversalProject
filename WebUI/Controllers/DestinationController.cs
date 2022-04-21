abdc6a2d1002e7d010a11f8deecd3df54af4af9d
zeljkosamed <smdkthyli@gmail.com>
2022-04-21 23:46:13 +0300

    Tur rotaları listelendi.

---
+++ WebUI/Controllers/DestinationController.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results.ComplexTypes;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Controllers
+{
+    public class DestinationController : Controller
+    {
+        private readonly IDestinationService _destinationService;
+
+        public DestinationController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var destinations = await _destinationService.GetAllAsync();
+            return View(new DestinationListDto
+            {
+                Destinations = destinations.Data.Destinations,
+                Message = $"asdkasdk"
+            });
+
+
+        }
+    }
+}
